441f871871343af7a41c7db2210bc2438c8c1c0b
Tatenda Samudzi <tatenda.samudzi@gmail.com>
2023-10-10 01:08:16 -0500

    Create mpu6050.py

---
+++ Midterm_Project/mpu6050.py
@@ -0,0 +1,39 @@
+import time
+import json
+from mpu6050 import mpu6050
+
+sensor = mpu6050(0x68)
+DELAY = 30
+
+
+def gen_file():
+    timestamp = time.strftime("%Y-%m-%d_%H-%M", time.localtime())
+    f = f"mpu6050_data_{timestamp}.json"
+    return f
+
+
+def r_s():
+    try:
+        while True:
+            f = gen_file()
+            accel = sensor.get_accel_data()
+            gyro = sensor.get_gyro_data()
+
+            sensor_data = {
+                "accelerometer": {"x": accel["x"], "y": accel["y"], "z": accel["z"]},
+                "gyro": {"x": gyro["x"], "y": gyro["y"], "z": gyro["z"]},
+            }
+
+            with open(f, "w") as json_file:
+                json.dump(sensor_data, json_file)
+                print("Saving to", f)
+
+            time.sleep(DELAY)
+
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == "__main__":
+    print("Reading data from MPU6050 and saving to JSON files.")
+    r_s()
